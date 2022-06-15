8154cd7cadd13783e1b3ff16084d44e049d4a003
stan-BK <2568143457@qq.com>
2022-06-15 17:30:32 +0800

    feat

--- index.js
+++ index.js
@@ -2,34 +2,52 @@
   window.draggableContent = function(el) {
     const wrap = document.querySelector(el)
     const children = wrap.children
-    let target = {
-      node: null,
-      cood: {
-        x: 0,
-        y: 0
-      }
-    }
-    for (var i = 0; i < children.length; i++) {
+    let dragItem = null
+    let offset = null
+    let isDrag = false
+    for (let i = 0; i < children.length; i++) {
       children[i].setAttribute('draggable', true)
       children[i].addEventListener('dragstart', dragStart)
       children[i].addEventListener('dragenter', dragEnter)
+      children[i].addEventListener('transitionend', () => isDrag = false)
     }
-    function dragStart() {
-      target.node = this
+    initElement(Array.from(children))
+    function initElement(elements) {
+      const arr = []
+      for (let i = 0; i < elements.length; i++) {
+        arr.push(elements[i].getBoundingClientRect().top)
+      }
+      offset = arr[0]
+      arr.forEach((top, idx) => {
+        const elem = elements[idx]
+        elem.style.position = 'absolute'
+        elem.style.top = top - offset + 'px'
+      })
+    }
+    function dragStart(e) {
+      dragItem = e.target
+      // e.dataTransfer.setDragImage('', 0, 0)
     }
     function dragEnter(e) {
       const tar = e.target
-      if (tar === target.node) {
+      if (tar === dragItem || isDrag === true) {
         return
       }
-      const children = Array.from(wrap.children)
-      const isNext = children.findIndex(item => item === target.node) < children.findIndex(item => item === tar)
-      if (isNext) {
-        wrap.insertBefore(tar, target.node)
+      isDrag = true
+
+      const { top: dragItemTop, height: dragItemHeight} = dragItem.getBoundingClientRect()
+      const { top: tarTop, height: tarHeight } = tar.getBoundingClientRect()
+      if (dragItemTop > tarTop) {
+        dragItem.style.top = tarTop - offset + 'px'
+        tar.style.top = dragItemTop + (dragItemHeight - tarHeight - offset)  + 'px'
       } else {
-        wrap.insertBefore(target.node, tar)
+        dragItem.style.top = tarTop + (tarHeight - dragItemHeight - offset) + 'px'
+        tar.style.top = dragItemTop - offset + 'px'
       }
     }
   }
 })()
-draggableContent('.wrap')
+window.onload = function() {
+  draggableContent('.wrap')
+
+}
