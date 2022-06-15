83a98d56b561b996d273f658ab3e846f5feda772
Stan-BK <2568143457@qq.com>
2022-06-15 21:11:23 +0800

    update

--- index.js
+++ index.js
@@ -1,22 +1,23 @@
 ;(function() {
-  const nodes = []
   window.draggableContent = function(el) {
     const wrap = document.querySelector(el)
+    const nodes = []
     const children = wrap.children
     let dragItem = null
     let offset = null
     let isDrag = false
     let height = 0
     for (let i = 0; i < children.length; i++) {
-      children[i].setAttribute('draggable', true)
+      children[i].setAttribute('draggable', 'true')
       children[i].addEventListener('dragstart', dragStart)
       children[i].addEventListener('dragenter', dragEnter)
       children[i].addEventListener('transitionend', () => isDrag = false)
       height += children[i].getBoundingClientRect().height
     }
+    window.addEventListener('resize', initElement(Array.from(children)))
     initElement(Array.from(children))
     wrap.style.height = height + 'px'
-    console.log(nodes)
+    
     function initElement(elements) {
       const arr = []
       for (let i = 0; i < elements.length; i++) {
@@ -29,33 +30,33 @@
         elem.style.top = top - offset + 'px'
         nodes[idx] = {}
         nodes[idx].node = elem
-        nodes[idx].top = elem.getBoundingClientRect().top
+        nodes[idx].top = parseInt(getComputedStyle(elem).top)
         nodes[idx].height = elem.getBoundingClientRect().height
       })
     }
+
     function dragStart(e) {
       dragItem = nodes.find(item => item.node === e.target)
-      // e.dataTransfer.setDragImage('', 0, 0)
     }
+    
     function dragEnter(e) {
+      e.preventDefault()
       const tar = e.target
-      if (tar === dragItem || isDrag === true) {
+      if (tar === dragItem.node || isDrag === true) {
         return
       }
       isDrag = true
-
-      // const { top: dragItemTop, height: dragItemHeight} = dragItem.getBoundingClientRect()
-      // const { top: tarTop, height: tarHeight } = tar.getBoundingClientRect()
       const node = nodes.find(item => item.node === tar)
-      console.log(dragItem, node)
       if (dragItem.top > node.top) {
-        dragItem.node.style.top = node.top + 'px'
-        tar.style.top = dragItem.top + (dragItem.height - node.height)  + 'px'
-        node.top = dragItem.top + (dragItem.height - node.height)
-        // dragItem.top = dragItem.top
+        const temp = node.top
+        dragItem.node.style.top = temp + 'px'
+        tar.style.top = (node.top = dragItem.top + (dragItem.height - node.height))  + 'px'
+        dragItem.top = temp
       } else {
-        dragItem.node.style.top = node.top + (node.height - dragItem.height) + 'px'
-        tar.style.top = dragItem.top + 'px'
+        const temp = dragItem.top
+        dragItem.node.style.top = (dragItem.top = node.top + (node.height - dragItem.height)) + 'px'
+        tar.style.top = temp + 'px'
+        node.top = temp
       }
     }
   }
